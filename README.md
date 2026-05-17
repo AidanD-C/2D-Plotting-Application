d2f48cc2805eed8ff057bbdf0b51f6382455abe3
AidanD-C <dc.aidan@gmail.com>
2026-05-17 14:45:12 -0400

    Setting Up Repo Structure

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # 2D-Plotting-Library
+The goal of this repo is to build a 2D plotting library using OpenGL capable of creating all sorts of different kinds of plots useful to physicists and mathematicians. Here's a list of capabilities I hope to implement:
+- vector field plotting
+- plotting scalar functions using color plots
+- plotting functions of x,y
+- plotting functions of x
+- ...
+
+## Dependencies
+
+This project requires GLFW. Download and compile GLFW from [glfw.org/download.html](https://www.glfw.org/download.html), then update the paths in `CMakeLists.txt` to point to your compiled library and headers.
+
+In `CMakeLists.txt`:
+```cmake
+include_directories(
+    "path to your GLFW include directory"
+    "include"
+)
+
+link_directories("path to your GLFW lib directory")
+```
---
+++ src/main.cpp
@@ -0,0 +1,92 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include "core\shader.h"
+#include <vector>
+
+const char* VERTEX_SHADER_PATH = "shaders/vertex_shader.txt";
+const char* FRAGMENT_SHADER_PATH = "shaders/fragment_shader.txt";
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+float vertices[] = {
+// positions // colors
+0.5f, -0.5f, 0.0f, // bottom right
+-0.5f, -0.5f, 0.0f, // bottom left
+0.0f, 0.5f, 0.0f, // top
+};
+
+int main()
+{   
+    // initializing GLFW and creating a window
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_SAMPLES, 4);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    glfwMakeContextCurrent(window);
+    
+    // initializing GLAD
+    if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glEnable(GL_MULTISAMPLE);
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    Shader basic_shader(VERTEX_SHADER_PATH, FRAGMENT_SHADER_PATH);
+
+    // creating a vertex array object which makes it more convenient to switch between different vertex data and attribute configurations. Also just makes binding vertex buffers easier.
+    unsigned int VAO;
+    unsigned int VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    // 1. bind Vertex Array Object
+    glBindVertexArray(VAO);
+    // 2. copy our vertices array in a buffer for OpenGL to use
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // 3. then set our vertex attributes pointers
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    // render loop
+    while(!glfwWindowShouldClose(window))
+    {
+        // input
+        processInput(window);
+
+        // rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        basic_shader.use();
+        // now render the triangle
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        // check and call events and swap the buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
